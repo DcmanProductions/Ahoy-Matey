8caba233fa53e69a9c386d0d984183a106774cda
Drew Chase <dcmanproductions@gmail.com>
2017-08-01 21:21:14 -0400

    Added The Environment Standard Assets and created a Network Manager

---
+++ AhoyMatey/Assets/Scripts/MyNetworkHud.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class MyNetworkHud : NetworkManager
+{
+
+    public void StartLocalHost()
+    {
+        Debug.Log("Starting Local Host AT " + Time.timeSinceLevelLoad);
+        StartHost();
+    }
+
+    public override void OnStartHost()
+    {
+        Debug.Log("Started Host At " + Time.timeSinceLevelLoad);
+    }
+
+}
